6a07eeb9410c46afbdf99f97faf065fef406d708
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-08 16:24:08 +0000

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "sort.h"
+ 
+/**
+*swap - swapea two elements of a array
+*@first: first number of array
+*@second: second number of array
+**/
+void swap(int *first, int *second)
+{
+	int temp = *first;
+	*first = *second;
+	*second = temp;
+}
+
+/**
+*selection_sort - as
+*@array: asdy
+*@size: asd
+**/
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j, min_idx;
+
+        for (i = 0; i < size-1; i++)
+        {
+                min_idx = i;
+                for (j = i+1; j < size; j++)
+                        if (array[j] < array[min_idx])
+                                min_idx = j;
+ 
+                swap(&array[min_idx], &array[i]);
+                print_array(array, size);
+        }
+}
