8f069133ed9eec09f3845c5754090aa0d8bb3433
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-06-08 12:00:19 -0300

    Fix 3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,3 @@
-// C program for implementation of Bubble sort
 #include <stdio.h>
 
 void swap(int* xp, int* yp)
@@ -12,8 +11,6 @@ void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
 	for (i = 0; i < size - 1; i++)
-
-		// Last i elements are already in place
 		for (j = 0; j < size - i - 1; j++)
 			if (&array[j] > &array[j + 1])
 				swap(&array[j], &array[j + 1]);
