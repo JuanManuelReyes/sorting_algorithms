a36220c53d6e1595782e2e580912160254453028
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-06-08 12:31:22 -0300

    Fixed 7

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,6 @@ void bubble_sort(int *array, size_t size)
             {
 				swap(&array[j], &array[j + 1]);
                 print_array(array, size);
-                break;
             }
         }
     }
