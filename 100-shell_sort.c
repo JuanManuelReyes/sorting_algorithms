67d7c9e37571e4a2017336f111e0c0caa6051980
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-13 15:21:49 +0000

    add changes

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,20 +1,25 @@
 #include "sort.h"
 void shell_sort(int *array, size_t size)
 {
-    int interval;
-    long unsigned int i;
-
-    for (interval = size / 2; interval > 0; interval /= 2) {
-    for (i = interval; i < size; i += 1) {
-      int temp = array[i];
-      int j;
-      for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
-        array[j] = array[j - interval];
-      }
-      array[j] = temp;
-    
-    }
-    print_array(array,size);
-  }
-  
+                long unsigned int in = 0, pos2 = 0;
+		long unsigned int h = 1;
+                int temp = 0;
+                 
+		h = (h * 3) + 1;
+		while (h > 0)
+		{
+			for (pos2 = h; pos2 < size; pos2++)
+			{
+				temp = array[pos2];   
+				in = pos2;        
+				while (in >= h && array[in - h] >= temp)
+				{
+					array[in] = array[in - h];
+					in = in - h;
+				}
+				array[in] = temp;
+			}
+			h = (h - 1) / 3;
+			print_array(array,size);
+		}
 }
