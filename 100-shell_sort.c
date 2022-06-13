cafa213c17efe4c1ef18cfeddcec7a8732a888ff
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-13 16:17:58 +0000

    fix betty 100

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,17 +1,23 @@
 #include "sort.h"
+/**
+ *shell_sort - the above function sorts an array
+ *of integers in ascending order using the Shell sort algorithm.
+ * @array: The array to be sorted
+ * @size: The size of the array
+ **/
 void shell_sort(int *array, size_t size)
 {
-                long unsigned int in = 0, pos2 = 0;
-		long unsigned int h = 1;
-                int temp = 0;
-                 
+		unsigned long int in = 0, pos2 = 0;
+		unsigned long int h = 1;
+		int temp = 0;
+
 		h = (h * 3) + 1;
 		while (h > 0)
 		{
 			for (pos2 = h; pos2 < size; pos2++)
 			{
-				temp = array[pos2];   
-				in = pos2;        
+				temp = array[pos2];
+				in = pos2;
 				while (in >= h && array[in - h] >= temp)
 				{
 					array[in] = array[in - h];
@@ -20,6 +26,6 @@ void shell_sort(int *array, size_t size)
 				array[in] = temp;
 			}
 			h = (h - 1) / 3;
-			print_array(array,size);
+			print_array(array, size);
 		}
 }
