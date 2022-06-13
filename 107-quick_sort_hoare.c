39c66a44363f0de4c8d2e01e274109b4eacef287
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-13 17:17:24 +0000

    add 107

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+*quick_sort_hoare - method counting sort
+*@array: array
+*@size: size
+**/
+void quick_sort_hoare(int *array, size_t size)
+{
+	print_array(array, size);
+}
