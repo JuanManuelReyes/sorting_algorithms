1a95aca134671fc17c4f46b1288a31c8ec355fb1
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-13 15:54:57 +0000

    fix identation in 2-select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -26,15 +26,13 @@ void selection_sort(int *array, size_t size)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 
-			if (min_idx != i)
-			{
-				swap(&array[min_idx], &array[i]);
-				print_array(array, size);
-			}
+		if (min_idx != i)
+		{
+			swap(&array[min_idx], &array[i]);
+			print_array(array, size);
 		}
 	}
 }
