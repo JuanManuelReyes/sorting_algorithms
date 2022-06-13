e2588b9474095b7cbb01d2eb6982b9bd32633fdf
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-13 16:47:02 +0000

    add 102

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,1 +1,13 @@
-printf();
+#include "sort.h"
+/**
+*counting_sort - method counting sort
+*@array: array
+*@size: size
+**/
+void counting_sort(int *array, size_t size)
+{
+    if (size < 2)
+    {
+        return;
+    }
+}
