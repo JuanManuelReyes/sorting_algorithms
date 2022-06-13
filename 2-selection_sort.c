8d1f4760ded69c57e58d4c525694552db37b9d16
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-13 15:41:28 +0000

    changes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -25,14 +25,14 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for (j = i+1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_idx])
-                min_idx = j;
-                
-            if (min_idx != i)
-            {
-                swap(&array[min_idx], &array[i]);
-                print_array(array, size);
-            }
-    }
+				min_idx = j;
+
+			         if (min_idx != i)
+			{
+				swap(&array[min_idx], &array[i]);
+				print_array(array, size);
+			}
+	}
 }
